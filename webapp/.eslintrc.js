3f4d2aa126839f4e0405be8a3e7092f085d16de8
Kenny Yeo <kenny@yeoyou.net>
2021-02-09 23:15:46 +1100

    feat: add eslint

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -3,21 +3,23 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: {
     browser: true,
     es6: true,
     jest: true,
   },
   plugins: ['@typescript-eslint', 'react'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2017,
-    sourceType: 'module',
-  },
   rules: {
     'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
     '@typescript-eslint/no-unused-vars': 'warn',
@@ -26,7 +28,11 @@ module.exports = {
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
     'no-console': 'warn',
     'max-len': ['warn', { code: 120 }],
-    'comma-dangle': ['error', 'always-multiline'],
+    /*
+    'comma-dangle': ["error", {
+      "functions": "never"
+    }],
+    */
     semi: [2, 'always'],
     // 'arrow-parens': ['error', 'always'],
     'no-new-object': 'error',
